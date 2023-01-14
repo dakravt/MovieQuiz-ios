2474a6e6dc228eaf584e9014a93f4889f6aa97a3
Danil <121294871+dakravt@users.noreply.github.com>
2023-01-14 14:53:18 +0300

    Add files via upload
    Поменян цвет бэкграунда на YP Black, как указано в макете
    Доприватил остальные функции и свойства
    Переделал функцию, отвечающую за блокировку кнопок при показе правильного ответа (убрал .toggle)
    Перенес свойста в класс
    Убрал неправильные коммиты из репозитория ЯП и создал в своем

---
+++ MovieQuiz-ios-project_sprint_3_start/MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -0,0 +1,147 @@
+import UIKit
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+
+struct QuizResultViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
+
+final class MovieQuizViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent  //делаем статус бар светлым
+    }
+    
+    // MARK: - Outlets
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var yesButton: UIButton!
+    
+    private var questions: [QuizQuestion] = [
+    QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+    QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+    QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+    QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+    QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+    QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+    QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+    QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+    QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+    ]
+
+    private var currentQuestionIndex: Int = 0
+    private lazy var currentQuestion = questions[currentQuestionIndex]
+    private var correctAnswers: Int = 0
+    
+    // MARK: - Functions
+    @IBAction private func noButtonClicked(_ sender: Any) {
+        deactivateButtons() //лочим кнопки
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    @IBAction private func yesButtonClicked(_ sender: Any) {
+        deactivateButtons() // лочим кнопки
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    private func activateButtons() { //функция делает кнопки активными
+        noButton.isEnabled = true
+        yesButton.isEnabled = true
+    }
+    
+    private func deactivateButtons() { //функция делает кнопки НЕактивными
+        noButton.isEnabled = false
+        yesButton.isEnabled = false
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        counterLabel.text = step.questionNumber
+        imageView.image = step.image
+        textLabel.text = step.question
+    }
+
+    private func show(quiz result: QuizResultViewModel) {
+        let alert = UIAlertController(title: result.title,
+                                      message: result.text,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: result.buttonText, style: .default, handler: { [self] _ in
+            currentQuestionIndex = 0
+            correctAnswers = 0
+            
+            let firstQuestion = questions[currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        })
+        
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    
+    private func showAnswerResult(isCorrect: Bool){
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 20
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResult()
+            self.imageView.layer.borderWidth = 0 // убираем рамку после каждого отвеченного вопроса
+        }
+    }
+    
+    private func showNextQuestionOrResult() {
+        if currentQuestionIndex == questions.count - 1 {
+            let resultText = "Ваш результат: \(correctAnswers) из \(questions.count)"
+            let viewModel = QuizResultViewModel(title: "Этот раунд окончен!",
+                                                text: resultText,
+                                                buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+            activateButtons() //анлочим кнопки
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            activateButtons() //анлочим кнопки
+            show(quiz: viewModel)
+        }
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        show(quiz:convert(model: currentQuestion))
+                
+    }
+}
+
+
+
